2c1f3c1696a6dd28c153d611dedc249bb5dcb7e5
soniyapatelQA <soniyapatel9109@gmail.com>
2024-04-24 16:11:51 +0100

    Java week1 writing is done

---
+++ src/homework_week_5/Programme_5_Tiangle.java
@@ -0,0 +1,24 @@
+package homework_week_5;
+
+/**
+ * 5. Write a Java Program to print below triangle
+ *            *
+ *           * *
+ *          * * *
+ *         * * * *
+ *        * * * * *
+ *       * * * * * *
+ */
+
+public class Programme_5_Tiangle {
+
+    public static void main(String[] args) {
+        System.out.println("       *       ");
+        System.out.println("      * *      ");
+        System.out.println("     * * *     ");
+        System.out.println("    * * * *    ");
+        System.out.println("   * * * * *   ");
+        System.out.println("  * * * * * *  ");
+    }
+
+}
